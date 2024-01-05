b3e99f12ec0f4df705a2ce4c795b706c2825aa66
Neil McLeish <neil.mcleish@tpximpact.com>
2024-01-05 17:16:25 +0000

    feat: add carousel stories

---
+++ src/components/Carousel/Carousel.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { v4 as uuidv4 } from 'uuid';
+import Carousel from './Carousel';
+import Card from '../Card/Card';
+
+const meta = {
+  title: 'Example/Carousel',
+  component: Carousel,
+  parameters: {
+    layout: 'centered'
+  },
+  tags: ['autodocs']
+} satisfies Meta<typeof Carousel>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const cards = Array(12)
+  .fill(null)
+  .map(() => <Card key={uuidv4()} {...Card.defaultProps} />);
+
+export const Primary: Story = {
+  args: {
+    children: cards
+  }
+};
